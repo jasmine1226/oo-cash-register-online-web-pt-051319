f967c518de685ffd66f2b6c617a794ddb9d41b9c
Jasmine Chen <jasmine1226@gmail.com>
2019-06-03 01:21:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,8 +6,8 @@ class CashRegister
     @discount = discount
   end
 
-  def add_item(title, price)
-    total = total + price
+  def add_item(title, price, quantity=1)
+    total = total + price * quantity
   end
 
 end
